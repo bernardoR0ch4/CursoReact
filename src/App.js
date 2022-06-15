2fe5be9eead3d99da4d7cbfac5c7d35f546db67c
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-06-15 14:53:34 -0300

    modificações

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = 'Maria'
 
-  const name = 'Bernardo'
-
-  const newName = name.toUpperCase()
-
-  function sum(a ,b){
-    return a + b 
-  }
-
-  const url = 'https://via.placeholder.com/150'
 
   return (
     <div className="App">
-     <h2>Alterando o JSX</h2>
-     <p>Olá, {newName}</p>
-     <p>Soma: {sum(1, 2)}</p>
-     <img src={url} alt="Minha Imagem"/>
-     <HelloWorld/>
+      <SayMyName nome="Bernardo" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150" 
+        />
     </div>
-  );
+  )
 }
 
 export default App;
